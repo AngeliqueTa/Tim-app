edbd1a5a4189ef74e84b78f3c1aff5d306097584
Angelique Taute <Angelique.Taute@britehouse.co.za>
2018-11-21 10:40:47 +0200

    Start up pages and build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/Home.component';
+import {RouterModule} from '@angular/router';
+
+import {HttpModule} from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+import { NavComponent } from './Nav/Nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    HttpModule,
+    AppRoutingModule
   ],
+  exports: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })

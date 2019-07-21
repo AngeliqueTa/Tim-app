9b058bb7a6a4064b87341f938221c12195dcfe18
Angelique Taute <Angelique.Taute@britehouse.co.za>
2019-07-21 11:23:43 +0200

    feat(Components): Added new folder and files and routing

---
+++ src/app/gis/gis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gis',
+  templateUrl: './gis.component.html',
+  styleUrls: ['./gis.component.css']
+})
+export class GISComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
